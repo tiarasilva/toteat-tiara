312661e69d29e7821713f7062048cfe8281f1fc8
Tiara Silva <tsilva7@uc.cl>
2022-07-16 22:48:55 -0400

    backend: modiciaciones para subir a heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,6 +134,6 @@ STATIC_URL = '/static/'
 os.makedirs(STATIC_TMP, exist_ok = True)
 os.makedirs(STATIC_ROOT, exist_ok = True)
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
